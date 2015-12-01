ca321b2f55ca3d12c13737402f81a0e27d2d7edc
kdko <kdko@email.wm.edu>
2015-11-30 21:57:52 -0500

    Projectiles now centered, cannon size is increased

--- bin/screen_1.hpp
+++ bin/screen_1.hpp
@@ -195,7 +195,7 @@ int screen_1::Run(sf::RenderWindow &App)
     	// left click...
 		if(exlaser.fire())
 			{
-				world.addItem(new Item(0, 0, exlaser.spd * cos(pc.orientation)+pc.vx,exlaser.spd * sin(pc.orientation)+pc.vy,3,0,10), 600+pc.lx,600+pc.ly);
+				world.addItem(new Item(0, 0, exlaser.spd * cos(pc.orientation)+pc.vx,exlaser.spd * sin(pc.orientation)+pc.vy,3,0,10), 607+pc.lx,607+pc.ly);
 			}
 	}
 	
@@ -204,7 +204,7 @@ int screen_1::Run(sf::RenderWindow &App)
     	// right click...
 		if(excannon.fire())
 			{
-				world.addItem(new Item(0, 0, excannon.spd * cos(pc.orientation)+pc.vx,excannon.spd * sin(pc.orientation)+pc.vy,3,10000,0), 600+pc.lx,600+pc.ly);
+				world.addItem(new Item(0, 0, excannon.spd * cos(pc.orientation)+pc.vx,excannon.spd * sin(pc.orientation)+pc.vy,7,10000,0), 607+pc.lx,607+pc.ly);
 			}
 	}
 
